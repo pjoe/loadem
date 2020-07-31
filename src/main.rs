6f74a531ef69bd9b2e5477372339cbcd61318c9c
Pelle Johnsen <pelle.johnsen@gmail.com>
2020-07-31 14:27:22 +0000

    Add test param

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ use std::{env, fs, io};
 use clap::{crate_version, App, Arg};
 use hyper::{body::HttpBody as _, client, Client};
 use simple_error::SimpleError;
+use tokio::io as tokio_io;
+use tokio::io::AsyncWriteExt as _;
 use tokio::sync::mpsc;
 use tokio::time::delay_for;
 
@@ -30,44 +32,61 @@ fn main() {
 }
 
 async fn loadem() -> Result<()> {
-    let matches = App::new(env!("CARGO_PKG_NAME"))
-        .about("Makes some load")
-        // use crate_version! to pull the version number
-        .version(crate_version!())
-        .arg(
-            Arg::with_name("URL")
-                .help("The url to test")
-                .required(true)
-                .index(1),
-        )
-        .arg(
-            Arg::with_name("CLIENTS")
-                .help("The number of clients")
-                .default_value("5")
-                .index(2),
-        )
-        .arg(
-            Arg::with_name("cert")
-                .long("cert")
-                .help("Custom certificate for HTTPS")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("time-limit")
-                .long("time-limit")
-                .short("l")
-                .help("Time limit. Test will end after specified number of seconds")
-                .default_value("0")
-                .takes_value(true),
-        )
-        .get_matches();
+    let matches =
+        App::new(env!("CARGO_PKG_NAME"))
+            .about("Makes continous load for web server testing.")
+            // use crate_version! to pull the version number
+            .version(crate_version!())
+            .arg(
+                Arg::with_name("URL")
+                    .help("The url to test")
+                    .required(true)
+                    .index(1),
+            )
+            .arg(
+                Arg::with_name("CLIENTS")
+                    .help("The number of clients")
+                    .default_value("5")
+                    .index(2),
+            )
+            .arg(
+                Arg::with_name("cert")
+                    .long("cert")
+                    .help("Custom certificate for HTTPS")
+                    .takes_value(true),
+            )
+            .arg(
+                Arg::with_name("time-limit")
+                    .long("time-limit")
+                    .short("l")
+                    .help("Time limit. Test will end after specified number of seconds")
+                    .default_value("0")
+                    .takes_value(true),
+            )
+            .arg(Arg::with_name("test").long("test").short("t").help(
+                "Test mode. No throughput measurements. Full response (with headers) is shown",
+            ))
+            .arg(
+                Arg::with_name("header")
+                    .long("header")
+                    .short("H")
+                    .help("Add HTTP request header(s)")
+                    .multiple(true)
+                    .takes_value(true),
+            )
+            .get_matches();
 
     pretty_env_logger::init();
 
     let url = matches.value_of("URL").unwrap();
     let url = url.parse::<hyper::Uri>().unwrap();
 
-    let clients = matches.value_of("CLIENTS").unwrap().parse::<u16>()?;
+    let test = matches.is_present("test");
+    let clients = if test {
+        1
+    } else {
+        matches.value_of("CLIENTS").unwrap().parse::<u16>()?
+    };
     let time_limit = matches.value_of("time-limit").unwrap().parse::<u64>()?;
 
     println!("URL: {}", url);
@@ -98,7 +117,7 @@ async fn loadem() -> Result<()> {
     let client: Client<_, hyper::Body> = Client::builder().build(https);
     let mut futures = vec![];
     for _ in 0..clients {
-        futures.push(fetch_url(&url, &client, tx.clone(), &QUIT));
+        futures.push(fetch_url(&url, &client, tx.clone(), test, &QUIT));
     }
 
     ctrlc::set_handler(move || {
@@ -108,7 +127,7 @@ async fn loadem() -> Result<()> {
 
     select! {
         _ = join_all(futures).fuse() => {}
-        _ = status(rx, &QUIT).fuse() => {}
+        _ = status(rx, test, &QUIT).fuse() => {}
         _ = heart_beat(tx.clone()).fuse() => {}
         _ = timeout(time_limit).fuse() => {}
     }
@@ -127,7 +146,7 @@ async fn timeout(limit: u64) -> Result<()> {
     Ok(())
 }
 
-async fn status(mut rx: mpsc::Receiver<Response>, quit: &AtomicBool) -> Result<()> {
+async fn status(mut rx: mpsc::Receiver<Response>, test: bool, quit: &AtomicBool) -> Result<()> {
     println!("Starting");
     let update_interval = Duration::from_secs(1);
     let mut now = SystemTime::now();
@@ -140,12 +159,14 @@ async fn status(mut rx: mpsc::Receiver<Response>, quit: &AtomicBool) -> Result<(
             now = SystemTime::now();
             let factor = 1f32 / elapsed.as_secs_f32();
             let total = count_ok + count_err;
-            println!(
-                "Tps {:>7.2}, Err {:>5.2}%, Resp Time {:>6.3}",
-                count_ok as f32 * factor,
-                count_err as f32 * 100f32 / total as f32,
-                resp_time / total as f32,
-            );
+            if !test {
+                println!(
+                    "Tps {:>7.2}, Err {:>5.2}%, Resp Time {:>6.3}",
+                    count_ok as f32 * factor,
+                    count_err as f32 * 100f32 / total as f32,
+                    resp_time / total as f32,
+                );
+            }
             count_ok = 0;
             count_err = 0;
             resp_time = 0f32;
@@ -186,15 +207,20 @@ async fn fetch_url(
     url: &hyper::Uri,
     client: &Client<hyper_rustls::HttpsConnector<hyper::client::HttpConnector>, hyper::Body>,
     mut tx: mpsc::Sender<Response>,
+    test: bool,
     quit: &AtomicBool,
 ) -> Result<()> {
-    // println!("fecthing: {}", url);
     while !quit.load(Ordering::Relaxed) {
         let start = SystemTime::now();
         let res = client.get(url.clone()).await;
         let status = match res {
             Ok(mut res) => {
                 let mut status: u16 = res.status().into();
+                if test {
+                    println!("Status: {}", status);
+                    println!("Headers: {:#?}\n", res.headers());
+                    println!("Body:");
+                }
                 // Stream the body, writing each chunk to stdout as we get it
                 // (instead of buffering and printing at the end).
                 while let Some(next) = res.data().await {
@@ -203,7 +229,9 @@ async fn fetch_url(
                         status = 901;
                         break;
                     }
-                    //io::stdout().write_all(&chunk).await?;
+                    if test {
+                        tokio_io::stdout().write_all(&chunk?).await?;
+                    }
                 }
 
                 status
@@ -218,9 +246,11 @@ async fn fetch_url(
         .await?;
 
         delay_for(Duration::from_micros(1)).await;
+        if test {
+            println!();
+            break;
+        }
     }
-
-    // println!("\n\nDone!");
 
     Ok(())
 }
